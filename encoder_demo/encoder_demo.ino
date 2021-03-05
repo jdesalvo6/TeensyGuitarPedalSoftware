0cfd67d23a2bed98a626bc372320c97726992fa6
jdesalvo6 <78983593+jdesalvo6@users.noreply.github.com>
2021-03-05 16:03:28 -0500

    Update encoder_demo.ino

--- encoder_demo/encoder_demo.ino
+++ encoder_demo/encoder_demo.ino
@@ -1,26 +1,21 @@
 #include <Bounce.h>
 
-#define PIN_ENCODER_L 3
 #define PIN_ENCODER_R 2
+#define PIN_ENCODER_L 3
+#define PIN_ENCODER_PUSH 33
  
 void setup()
 {
   // set pins as input with internal pull-up resistors enabled
   pinMode(PIN_ENCODER_L, INPUT_PULLUP);
   pinMode(PIN_ENCODER_R, INPUT_PULLUP);
+  pinMode(PIN_ENCODER_PUSH, INPUT_PULLUP);
   digitalWrite(PIN_ENCODER_L, HIGH);
   digitalWrite(PIN_ENCODER_R, HIGH);
+  //digitalWrite(PIN_ENCODER_PUSH, HIGH);
   Serial.begin(9600);
  
 }
-
-unsigned int countF = 0;            // how many times has it changed to low
-unsigned int countD = 0; 
-unsigned long countAtF = 0;         // when count changed
-unsigned long countAtD = 0;
-unsigned int countPrintedF = 0;
-unsigned int countPrintedD = 0;
-unsigned int mode = 0;
 
 void loop()
 {
@@ -33,5 +28,10 @@ void loop()
   {
     Serial.println("right");
     delay(75);
+  }
+  if (digitalRead(PIN_ENCODER_PUSH) == LOW)
+  {
+    Serial.println("push");
+    delay(300);
   }
 }
